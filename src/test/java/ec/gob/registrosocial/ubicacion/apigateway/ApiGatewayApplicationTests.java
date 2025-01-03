6c2eab2aff1c9a781372ae500c890c83709f29c8
Holger Neptali Morales Rivera <holger.morales@funcionjudicial.gob.ec>
2025-01-03 07:31:06 -0500

    feat: Implement gateway service Spring Cloud

---
+++ src/test/java/ec/gob/registrosocial/ubicacion/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.gob.registrosocial.ubicacion.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
